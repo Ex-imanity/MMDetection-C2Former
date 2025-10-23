a9cc2ec8525f942000cada8e004905c02409667d
imanity <15872128009@163.com>
2025-10-23 15:02:21 +0800

    同步server-DroneVehicle数据集

--- configs/cascade_rcnn/cascade_rcnn_r50_fpn_2x_drone_vehicle_ir.py
+++ configs/cascade_rcnn/cascade_rcnn_r50_fpn_2x_drone_vehicle_ir.py
@@ -17,7 +17,7 @@ model = dict(
                 num_classes=5)]))
 
 dataset_type = 'DroneVehicleIRDataset'
-data_root = 'D:/Imanity/dev/Data/DroneVehicle/DroneVehicle_VOCdevkit/VOC2012/'
+data_root = '/home/hclserver/zp/Data/VOC2012/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -46,8 +46,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=8,
+    workers_per_gpu=8,
     train=dict(
         type='RepeatDataset',
         times=3,
